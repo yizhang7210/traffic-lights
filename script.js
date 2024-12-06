723518515fd00dddc2a601bc32c46435193cd9bd
Yi Zhang <yizhang7210@users.noreply.github.com>
2024-12-05 23:50:28 -0800

    Implement lane changes

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ canvas.style.width = sizeWidth
 canvas.style.height = sizeHeight
 
 let state = 'NOT_STARTED'
+const INFINITY = 1000000
 
 // TODO: Make number of lanes configurable
 const LINE_MARGIN = 100
@@ -61,10 +62,27 @@ class Car {
   }
 }
 
+class LaneChangeSituation {
+  constructor (laneTo, insertAt, carSpeedInFront, distanceInFront) {
+    this.laneTo = laneTo
+    this.insertAt = insertAt
+    this.carSpeedInFront = carSpeedInFront
+    this.distanceInFront = distanceInFront
+  }
+
+  score () {
+    return this.distanceInFront / this.carSpeedInFront
+  }
+}
+
 class CarLane {
   constructor (y, cars_in_lane) {
     this.y = y
     this.cars = cars_in_lane
+    this.cars.sort((c1, c2) => {
+      // Sort by x position, right to left
+      return c2.x - c1.x
+    })
   }
 
   addCar (car) {
@@ -80,7 +98,16 @@ class CarLane {
     return new CarLane(this.y, this.cars.map(car => car.clone()))
   }
 
-  move () {
+  removeCarAt (index) {
+    this.cars = this.cars.slice(0, index).concat(this.cars.slice(index + 1))
+  }
+
+  insertCarAt (car, index) {
+    car.y = this.y
+    this.cars = this.cars.slice(0, index).concat([car]).concat(this.cars.slice(index))
+  }
+
+  move (adjacent_lanes) {
     if (state !== 'RUNNING') {
       return
     }
@@ -88,11 +115,6 @@ class CarLane {
     if (this.cars.length === 0) {
       return
     }
-
-    this.cars.sort((c1, c2) => {
-      // Sort by x position, right to left
-      return c2.x - c1.x
-    })
 
     // Stop when the last car crosses the line
     const lastIndex = this.cars.length - 1
@@ -103,7 +125,8 @@ class CarLane {
     }
 
     // Try to make every car move forward as fast as possible
-    for (let i = 0; i < this.cars.length; i++) {
+    let i = 0
+    while (i < this.cars.length) {
       const curr = this.cars[i]
 
       // update location based on speed
@@ -112,8 +135,68 @@ class CarLane {
       // update speed based on acceleration
       curr.current_speed = Math.max(0, Math.min(curr.top_speed, curr.current_speed + curr.current_acceleration))
 
+      // first car is always moving at top speed
       if (i === 0) {
         this.cars[0].current_speed = this.cars[0].top_speed
+        i += 1
+        continue
+      }
+
+      // Always an option to stay in the same lane
+      const carInFront = this.cars[i - 1]
+      let possibleLaneChanges = [new LaneChangeSituation(this, i, carInFront.current_speed, carInFront.x - curr.x - curr.length)]
+
+      // Explore changing lanes
+      for (const adjacent_lane of adjacent_lanes) {
+
+        // if no car in the next lane, definitely change lanes
+        if (adjacent_lane.size() === 0) {
+          possibleLaneChanges.push(new LaneChangeSituation(adjacent_lane, 0, INFINITY, INFINITY))
+          break
+        }
+
+        let j = 0
+        // Go through all the cars in the adjacent lane and try to find a spot
+        while (j < adjacent_lane.size()) {
+          // Try to find the first car behind with enough of a gap
+          if (adjacent_lane.cars[j].x + adjacent_lane.cars[j] + MAX_SAFE_DISTANCE >= curr.x) {
+            continue
+          }
+
+          if (j === 0) {
+            // Shouldn't happen - but the first car in the other lane is really behind
+            possibleLaneChanges.push(new LaneChangeSituation(adjacent_lane, 0, INFINITY, INFINITY))
+            break
+          }
+
+          const nextLaneCarInFront = adjacent_lane.cars[j - 1]
+
+          // if the car in front is far enough, there's a possibility of a lane change
+          if (curr.x + curr.length + MAX_SAFE_DISTANCE < nextLaneCarInFront.x) {
+            possibleLaneChanges.push(new LaneChangeSituation(adjacent_lane, j, nextLaneCarInFront.current_speed, nextLaneCarInFront.x - curr.x - curr.length))
+          }
+
+          // Either way, we won't find another spot, so will break
+          break
+        }
+
+        // if we're at the very end, that's also a possibility to change lanes
+        const nextLaneLastCar = adjacent_lane.cars[adjacent_lane.size() - 1]
+        if (curr.x + curr.length + MAX_SAFE_DISTANCE < nextLaneLastCar.x) {
+          possibleLaneChanges.push(new LaneChangeSituation(adjacent_lane, adjacent_lane.size(), nextLaneLastCar.current_speed, nextLaneLastCar.x - curr.x - curr.length))
+        }
+      }
+
+      possibleLaneChanges.sort((a, b) => {
+        return b.score() - a.score()
+      })
+
+      if (possibleLaneChanges[0].laneTo !== this) {
+        // Actually Change lanes
+        possibleLaneChanges[0].laneTo.insertCarAt(curr, possibleLaneChanges[0].insertAt)
+        this.removeCarAt(i)
+        // This car is done, move on to the next car
+        // Also don't increment i, since we just removed a car
         continue
       }
 
@@ -137,6 +220,7 @@ class CarLane {
       } else {
         curr.current_acceleration = 0
       }
+      i += 1
     }
   }
 
@@ -237,8 +321,16 @@ function drawBox () {
   ctx.clearRect(0, 0, canvas.width, canvas.height) // Clear canvas
   drawFinishingLine()
   drawLaneSeparator()
-  for (let lane of LANES) {
-    lane.move()
+  for (let i = 0; i < LANES.length; i++) {
+    lane = LANES[i]
+    let adjacent_lanes = []
+    if (i > 0) {
+      adjacent_lanes.push(LANES[i - 1])
+    }
+    if (i < LANES.length - 1) {
+      adjacent_lanes.push(LANES[i + 1])
+    }
+    lane.move(adjacent_lanes)
     lane.redraw()
   }
 
